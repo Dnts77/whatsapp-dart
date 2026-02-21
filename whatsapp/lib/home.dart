42431ad98d74b119254277d02d6586cda10f3164
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-02-21 12:23:43 -0300

    feat: TabBar adicionada com instância das novas telas

--- whatsapp/lib/home.dart
+++ whatsapp/lib/home.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: await_only_futures
+import 'package:whatsapp/screens/chats.dart';
+import 'package:whatsapp/screens/contacts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -8,9 +11,11 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
 
   String _emailUsuario = "";
+
+  TabController? _tabController;
 
   Future<void> _recuperarEmail() async{
     FirebaseAuth auth = FirebaseAuth.instance;
@@ -24,6 +29,7 @@ class _HomeState extends State<Home> {
   @override
   void initState(){
     _recuperarEmail();
+    _tabController = TabController(length: 2, vsync: this);
     super.initState();
   }
 
@@ -31,14 +37,28 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("WhatsApp"),
+        title: const Text("WhatsApp",style: TextStyle(fontWeight: FontWeight.bold)),
         backgroundColor: Color(0xff075e54),
         foregroundColor: Colors.white,
         elevation: 3.5,
         shadowColor: Colors.black,
+        bottom:  TabBar(
+          indicatorWeight: 4,
+          labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: [
+            const Text("Conversas", style: TextStyle(color: Colors.white),),
+            const Text("Contatos", style: TextStyle(color: Colors.white))
+          ],
+        ),
       ),
-      body: Container(
-        child: Text(_emailUsuario),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Chats(),
+          Contacts()
+        ],
       ),
     );
   }
