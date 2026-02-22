fd7fe35a88a14e01f58d71bdd5d1fb6649e007b4
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-02-22 14:02:35 -0300

    feat: Classe gerenciadora de rotas

---
+++ whatsapp/lib/routes/route_generator.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/home.dart';
+import 'package:whatsapp/login_screen.dart';
+import 'package:whatsapp/sign_up_screen.dart';
+
+
+class RouteGenerator {
+
+  static Route<dynamic> generateRoute(RouteSettings settings){
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(
+          builder: (_) => LoginScreen()
+        );
+      case "/login":
+        return MaterialPageRoute(
+          builder: (_) => LoginScreen()
+        );
+      case "/cadastro":
+        return MaterialPageRoute(
+          builder: (_) => SignUpScreen()
+        );
+      case "/home":
+        return MaterialPageRoute(
+          builder: (_) => Home()
+        );
+      default:
+        return _routeError();
+      
+    }
+    
+  }
+
+  static Route<dynamic> _routeError(){
+    return MaterialPageRoute(
+      builder: (_){
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Tela não encontrada!"),
+          ),
+          body: Center(
+            child: Text("Tela não encontrada!"),
+          ),
+        );
+      }
+    );
+  }
+
+}
