e2730a4e3ffd425fe33e472ea039b88253b77443
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-02-25 19:17:57 -0300

    feat: Listagem de contatos através dos usuários do firebase

--- whatsapp/lib/screens/contacts.dart
+++ whatsapp/lib/screens/contacts.dart
@@ -1,5 +1,10 @@
+//ignore_for_file: unused_import
+//ignore_for_file: unused_field
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:whatsapp/model/chat.dart';
+import 'package:whatsapp/model/user.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_user;
 
 class Contacts extends StatefulWidget {
   const Contacts({super.key});
@@ -10,63 +15,90 @@ class Contacts extends StatefulWidget {
 
 class _ContactsState extends State<Contacts> {
 
-  List<Chat> chatList = [
-    Chat(
-      "Ana Clara",
-      "Fala irmão",
-      "https://qsivrzekbkwddqqjdozd.supabase.co/storage/v1/object/sign/perfil/perfil1.jpg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8yODk3ZjE2Yi04NGIxLTRjMzQtOGI4Ny1hOWM3NWNiZTQ1MGMiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJwZXJmaWwvcGVyZmlsMS5qcGciLCJpYXQiOjE3NzE3MDMzOTQsImV4cCI6NDI5NDU4MzM5NH0.dsXenGhSgIPG2w5wm8NQdl2YfbTqyRSGjWkJlJBZAMc"
-    ),
-    
-    Chat(
-      "Pedro Silva",
-      "Me manda o link depois",
-      "https://qsivrzekbkwddqqjdozd.supabase.co/storage/v1/object/sign/perfil/perfil2.jpg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8yODk3ZjE2Yi04NGIxLTRjMzQtOGI4Ny1hOWM3NWNiZTQ1MGMiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJwZXJmaWwvcGVyZmlsMi5qcGciLCJpYXQiOjE3NzE3MDM0NzIsImV4cCI6NDI5NDU4MzQ3Mn0.MgxWJ5aVebRV3r6Uw7_iD4QBMQH8tvODs50KVa0aExs"
-    ),
-    
-    Chat(
-      "Marcela Almeida",
-      "Vamos fazer o orçamento",
-      "https://qsivrzekbkwddqqjdozd.supabase.co/storage/v1/object/sign/perfil/perfil3.jpg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8yODk3ZjE2Yi04NGIxLTRjMzQtOGI4Ny1hOWM3NWNiZTQ1MGMiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJwZXJmaWwvcGVyZmlsMy5qcGciLCJpYXQiOjE3NzE3MDM1MjcsImV4cCI6MTc3ODYxNTUyN30.oAdiq5z5X9jZoiNrTe0afhdKPCkOYczqK9sl9vtHvBg"
-    ),
-    
-    Chat(
-      "José Renato",
-      "Ele me mandou o contrato já",
-      "https://qsivrzekbkwddqqjdozd.supabase.co/storage/v1/object/sign/perfil/perfil4.jpg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8yODk3ZjE2Yi04NGIxLTRjMzQtOGI4Ny1hOWM3NWNiZTQ1MGMiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJwZXJmaWwvcGVyZmlsNC5qcGciLCJpYXQiOjE3NzE3MDM2MTksImV4cCI6NDI5NDU4MzYxOX0.zyGWjJ9_744qM4eXtwG5foqQY_wODIbRuA-C8QR4rXU"
-    ),
-    
-    Chat(
-      "Jamilton Damasceno",
-      "já já o uber chega",
-      "https://qsivrzekbkwddqqjdozd.supabase.co/storage/v1/object/sign/perfil/perfil5.jpg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8yODk3ZjE2Yi04NGIxLTRjMzQtOGI4Ny1hOWM3NWNiZTQ1MGMiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJwZXJmaWwvcGVyZmlsNS5qcGciLCJpYXQiOjE3NzE3MDM2ODEsImV4cCI6NDI5NDU4MzY4MX0._PVLv5v6xBHwjQ-XIfr2IKtXq7RbMvKCS5mWPMtPz1A"
-    ),
-    
-    
+  String? _loggedUserId;
+  String? _loggedUserEmail;
 
-  ];
+  Future<List<User>> _contactsRecovery() async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    QuerySnapshot querySnapshot = await db.collection("usuarios").get();
+
+    List<User> usersList = [];
+
+    for (DocumentSnapshot item in querySnapshot.docs) {
+      var dados = item.data() as Map<String, dynamic>;
+      if(dados["email"] == _loggedUserEmail) continue; //continue faz o parâmetro ser passado
+      User usuario = User();
+      usuario.email = dados["email"] ?? "";
+      usuario.nome = dados["nome"] ?? "Sem nome";
+      usuario.urlImage = dados["urlImagem"] ?? "";
+
+      usersList.add(usuario);
+    }
+    return usersList;
+  }
+
+  Future<void> _userDataRecovery() async {
+    firebase_user.FirebaseAuth auth = firebase_user.FirebaseAuth.instance;
+    firebase_user.User? loggedUser = auth.currentUser;
+    _loggedUserId = loggedUser!.uid;
+    _loggedUserEmail = loggedUser.email;
+    
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _userDataRecovery();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: chatList.length,
-      itemBuilder: (context, index){
-        Chat conversa = chatList[index];
-        return ListTile(
-          contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-          leading: CircleAvatar(
-            maxRadius: 30,
-            backgroundColor: Colors.grey,
-            backgroundImage: NetworkImage(conversa.caminhoFoto),
-          ),
-          title: Text(
-            conversa.nome,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 16
-            ),
-          ),
-        );
-      }
+    return FutureBuilder<List<User>>(
+      future: _contactsRecovery(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Center(
+              child: Column(
+                children: [
+                  Text("Carregando Contatos..."),
+                  CircularProgressIndicator(),
+                ],
+              ),
+            );
+
+          case ConnectionState.active:
+          case ConnectionState.done:
+            if(snapshot.hasError){
+              return Text("Erro ao carregar dados: ${snapshot.error}");
+            }
+            if(!snapshot.hasData || snapshot.data == null){
+              return Text("Nenhum dado encontrado");
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (_, index) {
+                List<User> itensList = snapshot.data!;
+                User usuario = itensList[index];
+                return ListTile(
+                  contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                  leading: CircleAvatar(
+                    maxRadius: 30,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: usuario.urlImage != null
+                        ? NetworkImage(usuario.urlImage!)
+                        : null,
+                  ),
+                  title: Text(
+                    usuario.nome!,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                );
+              },
+            );
+        }
+      },
     );
   }
 }
