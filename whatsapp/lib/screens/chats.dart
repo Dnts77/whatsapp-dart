10b8fe7dbdc67ecd4fe360c8b25e148dc95de5b1
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-03-02 16:28:33 -0300

    feat: Salvando & listando conversas do usuário

--- whatsapp/lib/screens/chats.dart
+++ whatsapp/lib/screens/chats.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/model/chat.dart';
-import 'package:supabase/supabase.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class Chats extends StatefulWidget {
   const Chats({super.key});
@@ -12,10 +15,14 @@ class Chats extends StatefulWidget {
 class _ChatsState extends State<Chats> {
 
   List<Chat> chatList =[];
+  final _controller = StreamController<QuerySnapshot>.broadcast();
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  String? _loggedUserId;
 
   @override
   void initState() {
     super.initState();
+    _userDataRecovery();
     Chat conversa = Chat();
     conversa.nome = "Ana Clara";
     conversa.mensagem = "Fala irmão";
@@ -24,35 +31,101 @@ class _ChatsState extends State<Chats> {
     chatList.add(conversa);
   }
 
+  Stream<QuerySnapshot> _addChatsListener(){
+    final stream = db.collection("conversas")
+    .doc(_loggedUserId)
+    .collection("ultima_conversa")
+    .snapshots();
+
+    stream.listen((dados){
+      _controller.add(dados);
+    });
+    
+    return stream;
+  }
+
+  Future<void> _userDataRecovery() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? loggedUser = auth.currentUser;
+    _loggedUserId = loggedUser!.uid;
+
+    _addChatsListener();
+    
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: chatList.length,
-      itemBuilder: (context, index){
-        Chat conversa = chatList[index];
-        return ListTile(
-          contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-          leading: CircleAvatar(
-            maxRadius: 30,
-            backgroundColor: Colors.grey,
-            backgroundImage: NetworkImage(conversa.caminhoFoto),
-          ),
-          title: Text(
-            conversa.nome,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 16
-            ),
-          ),
-          subtitle: Text(
-            conversa.mensagem,
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 16
-            )
-          ),
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: _controller.stream,
+      builder: (context, snapshot){
+        switch(snapshot.connectionState){
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Center(
+              child: Column(
+                children: [
+                  Text("Carregando conversas..."),
+                  CircularProgressIndicator(),
+                ],
+              ),
+            );
+          case ConnectionState.active:
+          case ConnectionState.done:
+            if (snapshot.hasError){
+              return Text("Erro ao carregar dados");
+            }
+            else{
+              QuerySnapshot querySnapshot = snapshot.data!;
+              if(querySnapshot.docs.isEmpty){
+                return Center(
+                  child: Text(
+                  "Você ainda não possui mensagens",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  )
+                );
+              }
+              return ListView.builder(
+                itemCount: chatList.length,
+                itemBuilder: (context, index){
+
+                List<DocumentSnapshot> conversas = querySnapshot.docs.toList();
+                DocumentSnapshot item = conversas[index];
+                String? urlImagem = item["caminhoFoto"];
+                String mensagem = item["mensagem"];
+                String nome = item["nome"];
+                  
+                  return ListTile(
+                    contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                    leading: CircleAvatar(
+                    maxRadius: 30,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: urlImagem != null ? NetworkImage(urlImagem)
+                    : null,
+                  ),
+                  title: Text(
+                  nome,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16
+                  ),
+                ),
+                subtitle: Text(
+                 mensagem,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16
+                )
+              ),
+            );
+          }
         );
       }
-    );
+    }
+  }
+);
+
+
+    
   }
 }
