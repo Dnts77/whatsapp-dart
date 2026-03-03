74f1d4f6f0cd067ec20c3ec40e79f3aae9565a26
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-03-03 19:02:19 -0300

    refactor: import inútil

--- whatsapp/lib/sign_up_screen.dart
+++ whatsapp/lib/sign_up_screen.dart
@@ -2,7 +2,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:whatsapp/home.dart';
 import 'package:whatsapp/model/user.dart' as user_model;
 
 class SignUpScreen extends StatefulWidget {
